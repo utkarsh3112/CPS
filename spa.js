73390d641ee87246d63383a5bf6ee7182dbab274
Utkarsh Sharma <62636987+utkarsh3112@users.noreply.github.com>
2021-11-20 23:56:05 +0530

    Add files via upload

---
+++ spa.js
@@ -0,0 +1,41 @@
+//Intialization parameters
+let country='in';
+let apiKey='c72d86e7ee124c51adb8d5db992560b0';
+//Grab the news Container
+let newsAccordian=document.getElementById("newsAccordion");
+//Create an ajax get request
+const xhr=new XMLHttpRequest();
+const result="https://newsapi.org/v2/top-headlines?country="+country+"&apiKey="+apiKey;
+xhr.open('GET', result, true);
+xhr.getResponseHeader('Content-type', 'application/json');
+
+// What to do when response is ready
+xhr.onload = function () {
+    if(this.status === 200){
+        let json=JSON.parse(this.responseText);
+        let articles=json.articles;
+        let newsHtml="";
+        var index=0;
+        for(var i=0;i<articles.length;i++){
+            var element=articles[i];
+            if(element.content==null){
+                element.content="";
+            }
+            else{
+                element.content=element.content+".";
+            }
+            if(element.urlToImage==null){
+                index--;
+                continue;
+            }
+            index++;
+            let news=`<div class="accordion-item"><h2 class="accordion-header" id="heading${index}"> <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapse${index}" aria-expanded="true" aria-controls="collapse${index}"><strong>Breaking News ${index}: &nbsp </strong>${element.title}</button></h2><div id="collapse${index}" class="accordion-collapse collapse collapsed" aria-labelledby="heading${index}" data-bs-parent="#accordionExample"><div class="accordion-body"><div class="img-container"><img src=${element.urlToImage} id="image_incident"></div><div class="text-container"><p><h4>${element.description}</h4></p>${element.content}<br><a href=${element.url} target="_blank">Read more here</a></div></div></div></div>`;
+            newsHtml+=news;
+        }       
+        newsAccordian.innerHTML=newsHtml;
+    }
+    else{
+        console.log("Some error occured")
+    }
+}
+xhr.send();
