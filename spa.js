d9645acda545d922c1d275438552110652b653b1
Utkarsh Sharma <62636987+utkarsh3112@users.noreply.github.com>
2021-12-01 20:05:41 +0530

    Update spa.js

--- spa.js
+++ spa.js
@@ -5,7 +5,8 @@ let apiKey='c72d86e7ee124c51adb8d5db992560b0';
 let newsAccordian=document.getElementById("newsAccordion");
 //Create an ajax get request
 const xhr=new XMLHttpRequest();
-const result="https://newsapi.org/v2/top-headlines?country="+country+"&apiKey="+apiKey;
+const result="https://gnews.io/api/v4/top-headlines?token=fdee7e386cdb503c1101964a76cfe631&country=in&lang=en";
+//"https://newsapi.org/v2/top-headlines?country="+country+"&apiKey="+apiKey;
 xhr.open('GET', result, true);
 xhr.getResponseHeader('Content-type', 'application/json');
 
@@ -23,12 +24,12 @@ xhr.onload = function () {
             }
             else{
                 element.content=element.content+".";
-            }
-            if(element.urlToImage==null){
+            }           
+            if(element.image==null){
                 continue;
             }
             index++;
-            let news=`<div class="accordion-item"><h2 class="accordion-header" id="heading${index}"> <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapse${index}" aria-expanded="true" aria-controls="collapse${index}"><span id="hide"><strong>Breaking News </span>${index}: &nbsp </strong>${element.title}</button></h2><div id="collapse${index}" class="accordion-collapse collapse collapsed" aria-labelledby="heading${index}" data-bs-parent="#accordionExample"><div class="accordion-body"><div class="img-container"><img src=${element.urlToImage} id="image_incident"></div><div class="text-container"><p><h4>${element.description}</h4></p>${element.content}<br><a href=${element.url} target="_blank">Read more here</a></div></div></div></div>`;
+            let news=`<div class="accordion-item"><h2 class="accordion-header" id="heading${index}"> <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapse${index}" aria-expanded="true" aria-controls="collapse${index}"><span id="hide"><strong>Breaking News </span>${index}: &nbsp </strong>${element.title}</button></h2><div id="collapse${index}" class="accordion-collapse collapse collapsed" aria-labelledby="heading${index}" data-bs-parent="#accordionExample"><div class="accordion-body"><div class="img-container"><img src=${element.image} id="image_incident"></div><div class="text-container"><p><h4>${element.description}</h4></p>${element.content}<br><a href=${element.url} target="_blank">Read more here</a></div></div></div></div>`;
             newsHtml+=news;
         }       
         newsAccordian.innerHTML=newsHtml;
